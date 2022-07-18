9d9322ab807fb4708f54e2255641c2d631f7b64c
paramjit singh <sparamjit92@gmail.com>
2022-07-18 15:36:46 +0530

    Add project files.

---
+++ MauiBugs/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiBugs;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		Picker picker = new Picker { Title = "Select Territory", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center };
+		picker.SetBinding(Picker.ItemsSourceProperty, "TerritoryNames", BindingMode.OneTime);
+		Content = picker;
+		BindingContext = new MainViewModel();
+	}
+
+
+}
+
---
+++ MauiBugs/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace MauiBugs
+{
+
+    public class MainViewModel
+    {
+        public IList<string> TerritoryNames { get; set; }
+        public MainViewModel()
+        {
+            TerritoryNames = new List<string>();
+            TerritoryNames.Add("Germany");
+        }
+
+    }
+}
