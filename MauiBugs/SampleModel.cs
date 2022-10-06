397fc32c1b9619d253c020171968f654b363e962
paramjit singh <sparamjit92@gmail.com>
2022-10-06 10:16:33 +0530

    Refactor

---
+++ MauiBugs/SampleModel.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace MauiBugs
+{
+   public class SampleModel
+    {
+        public string Question { get; set; }    
+        public string Answer { get; set; }
+
+        public bool IsAnswerBoxVisible { get; set; }
+
+
+    }
+}
