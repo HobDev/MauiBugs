a91bf94114edbdc4c08b95778899559176fd4a54
paramjit singh <sparamjit92@gmail.com>
2023-06-06 16:09:27 +0530

    Measure all items does not work on Windows sample

--- MauiBugs/MainViewModel.cs
+++ MauiBugs/MainViewModel.cs
@@ -1,14 +1,24 @@
 ﻿
 
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace MauiBugs
 {
 
-    public class MainViewModel
+    public partial class MainViewModel: ObservableObject
     {
-      
+        [ObservableProperty]
+        List<Person> persons;
+
         public MainViewModel()
         {
-            
+            persons = new List<Person>
+            {
+              
+                new Person{Name="Small Name"},
+                 new Person{Name="This is really a large Name"},
+                new Person{Name="This is Medium Name"}
+            };
         }
 
     }
